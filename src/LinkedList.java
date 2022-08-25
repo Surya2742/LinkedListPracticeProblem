346a43e3e928aff56d33cdc18ee3ccdc6abd226b
Surya2742 <boby7724284@gmail.com>
2022-08-26 00:09:51 +0530

    added: Functionality to append element/Integer in Linked List.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -17,6 +17,17 @@ public class LinkedList<K> {
         }
     }
 
+    public void append(INode<K> myNode) {
+        if (this.tail == null)
+            this.tail = myNode;
+        if (this.head == null) {
+            this.head = myNode;
+        } else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
+
     public void print() {
         INode<K> temp = head;
         while(temp != null) {
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> ll = new LinkedList<>();
-        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> firstNode = new Node<>(56);
         ll.add(firstNode);
         Node<Integer> secondNode = new Node<>(30);
-        ll.add(secondNode);
-        Node<Integer> thirdNode = new Node<>(56);
-        ll.add(thirdNode);
+        ll.append(secondNode);
+        Node<Integer> thirdNode = new Node<>(70);
+        ll.append(thirdNode);
         System.out.print("Linked List Sequence: ");
         ll.print();
     }
